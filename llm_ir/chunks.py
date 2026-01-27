f2c394d9998f31bf1e364740260758a7f2410e8f
mathisxy <mathis.siebert@gmail.com>
2026-01-27 19:47:23 +0000

    added RichReprMixin for replacing long lines with rprint with length value

--- llm_ir/chunks.py
+++ llm_ir/chunks.py
@@ -1,21 +1,22 @@
 from typing import Union, Literal
 from pydantic import BaseModel
+from .rich_repr import RichReprMixin
 
-class AIChunkText(BaseModel):
+class AIChunkText(RichReprMixin, BaseModel):
     type: Literal["text"] = "text"
     text: str
 
-class AIChunkFile(BaseModel):
+class AIChunkFile(RichReprMixin, BaseModel):
     type: Literal["file"] = "file"
     name: str
     mimetype: str
     bytes: bytes
 
-class AIChunkImageURL(BaseModel):
+class AIChunkImageURL(RichReprMixin, BaseModel):
     type: Literal["image"] = "image"
     url: str
 
-class AIChunkToolCall(BaseModel):
+class AIChunkToolCall(RichReprMixin, BaseModel):
     type: Literal["tool_call"] = "tool_call"
     id: str
     name: str
